61217af4dae6446ad199c056a6c200e1d5f74136
Peter <psugg92@gmail.com>
2018-03-06 09:26:54 -0600

    changed credientials

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -7,6 +7,7 @@ let pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
+    // changed again
 });
 
 function executeQuery(sql, args = []) {
